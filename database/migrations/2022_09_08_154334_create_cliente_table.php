26ec165b1f835800fdb7e3bfbf259a46faf49aa6
JPG <jpg.soporte@gmail.com>
2022-09-08 12:04:59 -0400

    Create DB tables trough laravel migrations. <<View should be created by DB client>>

--- database/migrations/2022_09_08_154334_create_cliente_table.php
+++ database/migrations/2022_09_08_154334_create_cliente_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cliente', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('empresa');
+            $table->string('sector');
+            $table->string('tam');
+            $table->unsignedBigInteger('asesor_id')->nullable();
+            $table->foreign('asesor_id')->references('id')->on('asesor')->nullOnDelete();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('cliente');
     }
 };
